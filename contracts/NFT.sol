f3e559e2b65ef2866aa751142a67b06875318f01
John Paul Cas <casjp.app@gmail.com>
2022-02-19 23:16:30 +0800

    🎨 change impl to ERC721Enumerable

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,30 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.6;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract NFT is ERC721, Ownable {
+contract NFT is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
 
     string public baseURI;
 
-    Counters.Counter private _nextTokenId;
     mapping(address => bool) public trustedOperators;
 
     constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
     {
-        _nextTokenId.increment();
-    }
-
-    /**
-     * @dev _nextTokenId ahead +1 so to get the total supply of token it must be -1
-     */
-    function totalSupply() public view returns (uint256) {
-        return _nextTokenId.current() - 1;
+        for (uint256 token = 0; token < 3; token++) {
+            _safeMint(_msgSender(), totalSupply() + 1);
+        }
     }
 
     /**
@@ -39,6 +33,10 @@ contract NFT is ERC721, Ownable {
     {
         require(_exists(_tokenId), "Token does not exist.");
         return string(abi.encodePacked(baseURI, Strings.toString(_tokenId)));
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
     }
 
     function isApprovedForAll(address _owner, address operator)
@@ -57,17 +55,15 @@ contract NFT is ERC721, Ownable {
      * @param _to The address where to mint the token
      */
     function mintTo(address _to) external onlyOwner {
-        uint256 currentTokenId = _nextTokenId.current();
-        _nextTokenId.increment();
-        _safeMint(_to, currentTokenId);
+        _safeMint(_to, totalSupply() + 1);
     }
 
     /**
      * @dev Set base URI
-     * @param _baseURI the new baseURI
+     * @param baseURI_ the new baseURI
      */
-    function setBaseURI(string memory _baseURI) public onlyOwner {
-        baseURI = _baseURI;
+    function setBaseURI(string memory baseURI_) public onlyOwner {
+        baseURI = baseURI_;
     }
 
     /**
